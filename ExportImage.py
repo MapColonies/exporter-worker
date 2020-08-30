e205092bd6ee7c0d74e7e3b84ee243ab1d223c38
Shlomi Koncha <shlomikoncha@gmail.com>
2020-08-30 18:23:57 +0300

    added kafka-python support

--- ExportImage.py
+++ ExportImage.py
@@ -1,5 +1,7 @@
 from osgeo import gdal
 import configparser
+import random
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 outputSRS = config['io']['outputSRS']
@@ -7,7 +9,7 @@ outputFormat = config['io']['outputFormat']
 
 def export():
     kwargs = {'dstSRS': outputSRS, 'format': outputFormat, 'outputBounds': [34.812883, 31.907806, 34.814606, 31.909520]}
-    gdal.Warp('warp_test.gpkg',"WMS:http://localhost:8080/geoserver/cite/wms?service=WMS&version=1.1.0&request=GetMap&layers=cite:wizzman",**kwargs)
+    gdal.Warp(f'{random.randint(1,5555555)}.gpkg', "WMS:http://localhost:8080/geoserver/cite/wms?service=WMS&version=1.1.0&request=GetMap&layers=cite:wizzman", **kwargs)
     print('done')
 
 
