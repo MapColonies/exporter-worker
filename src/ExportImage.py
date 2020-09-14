a15d84ee570a0c3bdfe75b571ee18fec8b490bd4
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2020-09-14 17:26:40 +0300

    feat: create exporter skeleton (#7)
    * added GDAL support
    
    * added config
    
    * added kafka-python support
    
    * added kafka-consumer
    
    * add output file uuid
    
    * fixed export parameters
    
    * fixed kafkamq & skeleton
    
    * fixed typo
    
    Co-authored-by: Shlomi Koncha <shlomikoncha@gmail.com>

---
+++ src/ExportImage.py
@@ -0,0 +1,23 @@
+from osgeo import gdal
+import configparser
+from math import floor
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+outputSRS = config['io']['outputSRS']
+outputFormat = config['io']['outputFormat']
+
+
+def export(bbox, filename, url):
+    try:
+        kwargs = {'dstSRS': outputSRS, 'format': outputFormat, 'outputBounds': bbox, 'callback': progress_callback}
+        result = gdal.Warp(f'{filename}.gpkg', url, **kwargs)
+        return result
+    except Exception as e:
+        print(f'Failed export image: {e}')
+
+
+def progress_callback(complete, message, unknown):
+    percent = floor(complete*100)
+    print('progress: {}, message: "{}", unknown {}'.format(percent, message, unknown))
+    return percent
