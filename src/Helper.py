a62e561f1ead37369b52be74af2404b9f81940a8
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2020-09-15 13:26:31 +0300

    added deafult-config (#9)
    Co-authored-by: Shlomi Koncha <shlomikoncha@gmail.com>

--- src/Helper.py
+++ src/Helper.py
@@ -1,8 +1,7 @@
 import json
 
 
-class Helper():
+class Helper:
     def load_json(self, task):
         parsed_json = json.loads(task.value)
         return parsed_json
-
--- src/TaskHandler.py
+++ src/TaskHandler.py
@@ -1,26 +1,29 @@
-from pathlib import Path
 from kafka import KafkaConsumer
 from kafka.coordinator.assignors.roundrobin import RoundRobinPartitionAssignor
-from src import ExportImage
+from src.ExportImage import ExportImage
 from src.Helper import Helper
+from os import path
+import json
 
 
-class TaskHandler():
+class TaskHandler:
     def __init__(self):
         self.__helper = Helper()
-
-    #def _filename_exist(filename):
-    #    file = Path(f'{filename}.gpkg')
-    #    return file.exists()
+        self.__exportImage = ExportImage()
+        current_dir_path = path.dirname(__file__)
+        config_path = path.join(current_dir_path, '../config/default.json')
+        with open(config_path, encoding='utf-8') as config_file:
+            self.__config = json.loads(config_file.read())
 
     def handle_tasks(self):
-        consumer = KafkaConsumer(bootstrap_servers=['10.28.11.49:9092'], enable_auto_commit=False,
-                                 auto_offset_reset='latest', group_id='task_group_1',
+        consumer = KafkaConsumer(bootstrap_servers=[self.__config['kafka']['host_ip']], enable_auto_commit=self.__config['kafka']['auto_commit'],
+                                 auto_offset_reset=self.__config['kafka']['offset_reset'], group_id=self.__config['kafka']['group_id'],
                                  partition_assignment_strategy=[RoundRobinPartitionAssignor])
         try:
-            consumer.subscribe(['topic-test-5'])
+            consumer.subscribe([self.__config['kafka']['topic']])
             for task in consumer:
                 result = self.execute_task(task)
+                print(result)
                 if result is not None:
                     consumer.commit()
                     print('Task done.')
@@ -34,12 +37,4 @@ class TaskHandler():
         task_values = self.__helper.load_json(task)
         print('Task received, Offset: ', task.offset)
 
-        return ExportImage.export(task_values['bbox'], task_values['filename'], task_values['url'])
-
-
-
-
-
-
-
-
+        return self.__exportImage.export(task_values['bbox'], task_values['filename'], task_values['url'])
