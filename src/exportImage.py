a364e36427382f2f8bd3ddd652738318e8478ca8
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2020-09-22 18:04:51 +0300

    chore: mapping new files in output folder (#26)
    Co-authored-by: Shlomi Koncha <shlomikoncha@gmail.com>

--- src/exportImage.py
+++ src/exportImage.py
@@ -13,7 +13,7 @@ class ExportImage:
         try:
             self.logger.info(f'Task no.{offset} in progress.')
             kwargs = {'dstSRS': self.__config['input_output']['output_srs'], 'format': self.__config['input_output']['output_format'], 'outputBounds': bbox, 'callback': self.progress_callback}
-            result = gdal.Warp(f'{filename}.gpkg', url, **kwargs)
+            result = gdal.Warp(f'./outputs/{filename}.gpkg', url, **kwargs)
             return result
         except Exception as e:
             self.logger.error(f'Error occurred while exporting: {e}.')
