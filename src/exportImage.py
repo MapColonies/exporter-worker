7b92fc4a75ecadda6e4959bb3d5c6d2ba6512b6a
CL-SHLOMIKONCHA <cl_shlomikoncha@rnd-hub.com>
2020-10-12 17:23:57 +0300

    chore: added common-storage update support

--- src/exportImage.py
+++ src/exportImage.py
@@ -2,6 +2,9 @@ from osgeo import gdal
 from math import floor
 from log.logger import Logger
 from src.config import read_config
+import requests
+from datetime import datetime
+import json
 
 
 class ExportImage:
@@ -9,10 +12,14 @@ class ExportImage:
         self.logger = Logger()
         self.__config = read_config()
 
-    def export(self, offset, bbox, filename, url):
+    def export(self, offset, bbox, filename, url, taskid):
         try:
             self.logger.info(f'Task no.{offset} in progress.')
-            kwargs = {'dstSRS': self.__config['input_output']['output_srs'], 'format': self.__config['input_output']['output_format'], 'outputBounds': bbox, 'callback': self.progress_callback}
+            kwargs = {'dstSRS': self.__config['input_output']['output_srs'],
+                      'format': self.__config['input_output']['output_format'],
+                      'outputBounds': bbox,
+                      'callback': self.progress_callback,
+                      'callback_data': taskid}
             result = gdal.Warp(f'{self.__config["input_output"]["folder_path"]}/{filename}.gpkg', url, **kwargs)
             return result
         except Exception as e:
@@ -20,10 +27,27 @@ class ExportImage:
             raise e
 
     def progress_callback(self, complete, message, unknown):
-        percent = floor(complete*100)
-        #TODO:
-        # Add database connection to update with progress
-        self.logger.info(f'Updated database with progress: {percent}')
-        return percent
+        try:
+            percent = floor(complete * 100)
+            headers = {"Content-Type": "application/json"}
+            doc = {
+                "body": {
+                    "taskId": unknown,
+                    "status": "in-progress",
+                    "percent": percent
+                }
+            }
+            json.dumps(doc)
+            r = requests.post(url='http://10.28.11.49:8080/indexes/es/document', data=json.dumps(doc), headers=headers)
+            res = r.text
+            print(res)
+
+            if percent == 100:
+                doc['status'] = 'completed'
+            self.logger.info(f'Task Id "{unknown}" Updated database with progress: {percent}')
+            return percent
+        except Exception as e:
+            print(e)
+            raise e
 
 
